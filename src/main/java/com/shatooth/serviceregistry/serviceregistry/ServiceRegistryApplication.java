1fabe902b0ed4c5353cf979fa2283949d51ac419
debanjan <debanjan@nitsolution.in>
2023-09-23 13:35:39 +0530

    Docker file added

--- src/main/java/com/shatooth/serviceregistry/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/shatooth/serviceregistry/serviceregistry/ServiceRegistryApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.Optional;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
@@ -11,5 +13,4 @@ public class ServiceRegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceRegistryApplication.class, args);
     }
-
 }
